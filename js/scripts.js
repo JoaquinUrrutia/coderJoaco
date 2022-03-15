a7cdbe47cad700c48669b1cf0bec4883499866ce
Joaquin Urrutia <joaquinjurrutia@gmail.com>
2022-03-15 14:03:09 -0300

    subida del index

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+let listaBarra = document.querySelector('#listaBarra')
+
+let menuBarra = document.querySelector('#menuBarra')
+
+    menuBarra.addEventListener('click', function() {
+    
+    listaBarra.classList.toggle('barra-toggle');
+    
+    })
